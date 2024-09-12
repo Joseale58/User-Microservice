1aa593f6f82c16e862608de66c5a2dd86e383403
Jose Rivillas <josearivillas64@gmail.com>
2024-09-12 11:45:26 -0500

    feature(hu8): add testing unit

--- user-service/src/main/java/com/emazon/user_service/domain/model/Login.java
+++ user-service/src/main/java/com/emazon/user_service/domain/model/Login.java
@@ -16,4 +16,12 @@ public class Login {
     public String getPassword() {
         return password;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
